dabe087c9c2634c530c8431fee2e01a30e33e50d
elogon <elogon@gmail.com>
2020-02-18 15:47:01 -0500

    QT-10 Added new class for person model and changed code to use it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,61 @@
 import { Component } from '@angular/core';
 import * as $ from 'jquery';
 
+class Person {
+  name: string;
+  lastname: string;
+  dateOfBirth: string;
+  phoneNumber: string;
+  gender: string;
+  education: string;
+  isSmoking: string;
+  isAgreed: string;
+  correct: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  name: string = '';
-  lastname: string = '';
-  dateOfBirth: string = '';
-  phoneNumber: string = '';
-  gender: string = '';
-  education: string = '';
-  isSmoking: string = '';
-  isAgreed: string = '';
-  correct: boolean = false;
+  person: Person = new Person();
+  tableData: Person[] = [];
 
   startChecks() {
     // Reset all errors
     $(".msg").css("display", "none");
     $(".box").css("borderColor", "initial");
     $('#checkMsg').css('visibility', 'hidden');
-    this.correct = true;
+    this.person.correct = true;
 
     // get all Form data - all fields
-    this.name = $("#firstname").val().toString();
-    this.lastname = $("#lastname").val().toString();
-    this.dateOfBirth  = $("#dob").val().toString();
-    this.phoneNumber  = $("#phone").val().toString();
-    this.education = $("#education>option:selected").text()
-    this.isAgreed  = ($('#agreement:checked').val() || '').toString();
+    this.person.name = $("#firstname").val().toString();
+    this.person.lastname = $("#lastname").val().toString();
+    this.person.dateOfBirth  = $("#dob").val().toString();
+    this.person.phoneNumber  = $("#phone").val().toString();
+    this.person.education = $("#education>option:selected").text()
+    this.person.isAgreed  = ($('#agreement:checked').val() || '').toString();
     // Get gender
-    this.gender = ($("input[name='gender']:checked").val() || '').toString();
+    this.person.gender = ($("input[name='gender']:checked").val() || '').toString();
     // Get Smoking or not
-    this.isSmoking = ($("input[name='smoking']:checked").val() || '').toString();
+    this.person.isSmoking = ($("input[name='smoking']:checked").val() || '').toString();
 
     // Check all form data if all feilds entered
-    this.checkElement(this.name,'.name');
-    this.checkElement(this.lastname,'.lastname');
-    this.checkElement(this.dateOfBirth,'.dob');
-    this.checkElement(this.phoneNumber,'.phone');
-    this.checkElement(this.gender,'.gender');
-    this.checkElement(this.education,'.education');
-    this.checkElement(this.isSmoking,'.smoking');
-    if (this.isAgreed) {
+    this.checkElement(this.person.name,'.name');
+    this.checkElement(this.person.lastname,'.lastname');
+    this.checkElement(this.person.dateOfBirth,'.dob');
+    this.checkElement(this.person.phoneNumber,'.phone');
+    this.checkElement(this.person.gender,'.gender');
+    this.checkElement(this.person.education,'.education');
+    this.checkElement(this.person.isSmoking,'.smoking');
+    if (this.person.isAgreed) {
         $('.icon-question').css('color', 'black');
     } else {
       this.checkElement('','.agree');
         $('.icon-question').css('color', 'red');
     }
-    if (this.correct) {
+    if (this.person.correct) {
         $('.msg-ok').css('visibility', 'visible');
     }
   }
@@ -61,7 +65,7 @@ export class AppComponent {
     if (el === '') {
         $(cls + '-err').css('display', 'flex')
         $(cls).css('borderColor', 'red')
-        this.correct = false;
+        this.person.correct = false;
     }
   };
 

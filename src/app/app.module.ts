98c6d24b1723b62ffc9931f8f93506e802f0b6b5
elogon <elogon@gmail.com>
2020-02-19 11:17:08 -0500

    QT-2 Added initial layout for table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabledataComponent } from './tabledata/tabledata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabledataComponent,
   ],
   imports: [
     BrowserModule

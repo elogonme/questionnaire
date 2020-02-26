bcc4553bcec51aa6198b90c44c88d9f47b25bdb4
Yevgeniy Huseynov <yevgeniyh@starstechnologies.com>
2020-02-26 16:06:11 -0500

    Do not merge this code. This is just an example for you. Review it please

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,56 +7,7 @@ import { Person } from './models/person';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   person: Person = new Person();
-  correct = false;
 
-  isFistNameValid = true;
-  isLastNameValid = true;
-  isDoBValid = true;
-  isPhoneValid = true;
-  isGenderValid = true;
-  isSmokingValid = true;
-  isEducationValid = true;
-  isAgreedValid = true;
-
-
-  startChecks() {
-
-    // Check all form data if all fields entered
-    this.isFistNameValid = this.checkMinimumLength(this.person.firstName,  3);
-    this.isLastNameValid = this.checkMinimumLength(this.person.lastName,  3);
-    this.isDoBValid = this.checkValidDate(this.person.dateOfBirth);
-    this.isPhoneValid = this.checkMinimumLength(this.person.phoneNumber, 10);
-    this.isGenderValid = this.checkNotNull(this.person.gender);
-    this.isEducationValid = this.checkMinimumNumber(this.person.education, 1);
-    this.isSmokingValid = this.checkNotNull(this.person.isSmoking);
-    this.isAgreedValid = this.checkNotNull(this.person.isAgreed);
-
-
-    this.correct = this.isFistNameValid
-      && this.isLastNameValid
-      && this.isDoBValid
-      && this.isPhoneValid
-      && this.isGenderValid
-      && this.isEducationValid
-      && this.isSmokingValid
-      && this.isAgreedValid;
-  }
-
-  // Check element for error, make red box and display error message
-  private checkMinimumLength(data: string, minimumLength: number = 1): boolean {
-    return data.length >= minimumLength;
-  }
-
-  private checkMinimumNumber(data: number, minimumNumber: number = 1): boolean {
-    return data >= minimumNumber;
-  }
-
-  private checkValidDate(data: Date): boolean {
-      return data !== undefined;
-  }
-
-  private checkNotNull(data: any | null): boolean {
-        return data !== null;
-  }
 }
--- src/app/models/person.ts
+++ src/app/models/person.ts
@@ -7,4 +7,15 @@ export class Person {
   education = 0;
   isSmoking: boolean | null = null;
   isAgreed: boolean | null = null;
+
+  validate(): boolean {
+    return this.firstName.length >= 3
+      && this.lastName.length >= 3
+      && (this.dateOfBirth !== undefined || this.dateOfBirth.toString() === 'Invalid Date')
+      && this.phoneNumber.length >= 10
+      && !!this.gender
+      && this.education > 1
+      && !!this.isSmoking
+      && !!this.isAgreed;
+  }
 }

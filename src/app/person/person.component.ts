81cb89820137645130208e54ead98f62b2f311ac
elogon <elogon@gmail.com>
2020-04-05 17:31:01 +0400

    QT-16 Add person service CRUD operations. Added unsubscribe where nessesary. Implemented move rows in list.

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 import { Person } from '../models/person';
-import { EDUCATION_MAP } from '../models/person';
 import { PersonService } from '../person.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-person',
@@ -11,38 +10,91 @@ import { PersonService } from '../person.service';
 })
 
 export class PersonComponent implements OnInit {
-  person: Person = new Person();
-  persons: Person[];
-  educationMap = EDUCATION_MAP;
-  personsTable: MatTableDataSource<Person> = new MatTableDataSource<Person>();
-  slectedPersonIndex: number;
+
+  person: Person = new Person(); // this is data for PersonFormComponent
+  persons: Person[] = []; // this is data for PersonListComponent
+  private subscription: Subscription;
 
   constructor(private personService: PersonService) {}
 
   ngOnInit() {
-    this.personService.getAllPersons();
-    this.personService.persons.subscribe(
-      persons => {
-      this.persons = persons;
-      this.personsTable.data = persons;
-    });
+    this.getAllPersons();
   }
 
-  addUpdatePerson(person) {
-    if (person.action) {                // Add new person is action is true
-      this.personService.addPerson(this.person);
-      this.persons = this.persons.slice();
-    } else {                             // Update selected person is action is false
-      this.personService.updatePerson(this.person);
-      this.persons = this.persons.slice();
-      this.slectedPersonIndex = -1;
-      this.person = new Person();
-    }
+  getAllPersons(): void {
+    this.subscription = this.personService.getAllPersons().subscribe(
+      persons => this.persons = persons,
+      err => console.error(`Can not get all persons. error ${err}`),
+      () => console.log(`List of persons retrieved. length=${this.persons.length}`)
+    );
+    this.subscription.unsubscribe();
   }
 
-  editPerson(person: any) {
-    this.person = Object.assign(new Person(), person.person);
-    this.slectedPersonIndex = person.index;
-    this.personService.updateObservables();
+  // event handler for PersonFormComponent
+  addPerson(person: Person): void {
+    let newId = '';
+    this.subscription = this.personService.addPerson(person).subscribe(
+      p => {
+        newId = p.id;
+        this.person = new Person();
+        this.getAllPersons();
+      },
+      err => console.error(`Can not add person. error ${err}`),
+      () => console.log(`Person added successfully. id=${newId}`)
+    );
+    this.subscription.unsubscribe();
+  }
+
+  // event handler for PersonFormComponent
+  updatePerson(person: Person): void {
+    this.subscription = this.personService.updatePerson(person).subscribe(
+      result => {
+        if (result) {
+          this.person = new Person();
+          this.getAllPersons();
+        } else {
+          console.error(`Can not update person. id=${person.id}`);
+        }
+      },
+      err => console.error(`Service error ${err}`),
+      () => console.log(`Person updated successfully. id=${person.id}`)
+    );
+    this.subscription.unsubscribe();
+  }
+
+  deletePerson(person: Person): void {
+    this.subscription = this.personService.deletePersonById(person.id).subscribe(
+      result => {
+        if (result) {
+          this.getAllPersons();
+        } else {
+          console.error(`Can not delete person. id=${person.id}`);
+        }
+      },
+      err => console.error(`Service error ${err}`),
+      () => console.log(`Person deleted successfully. id=${this.person.id}`)
+    );
+    this.subscription.unsubscribe();
+    this.person = new Person();
+  }
+
+  selectPerson(person: Person): void {
+    this.person = person.clone();
+  }
+
+  shiftPerson(personIdAndshift) {
+    this.subscription = this.personService.shiftPerson(personIdAndshift).subscribe(
+      result => {
+        if (result) {
+          this.getAllPersons();
+        } else {
+          console.error(`Can not shift person. id=${this.persons[personIdAndshift.id].id}`);
+        }
+      },
+      err => console.error(`Service error ${err}`),
+      () => console.log(`Person moved successfully. id=${this.persons[personIdAndshift.id].id}`)
+    );
+    this.subscription.unsubscribe();
+    this.person = new Person();
   }
 }
